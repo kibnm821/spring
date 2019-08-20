b218a211a837e7de645a9599e86044076d01d325
kibnm821 <kibnm821@naver.com>
2019-08-20 10:55:29 +0900

    Spring

---
+++ src/main/java/com/example/spring/controller/Login_Logout.java
@@ -0,0 +1,14 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class Login_Logout {
+    @RequestMapping(value = "/login")
+    public void Action_Login(){
+    }
+    @RequestMapping(value = "/logout")
+    public void Action_Logout(){
+    }
+}
