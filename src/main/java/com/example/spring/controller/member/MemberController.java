ad6709013c4fd4a9962c54b224ff23f819b99ce9
kibnm821 <kibnm821@naver.com>
2019-08-21 17:50:17 +0900

    Spring

--- src/main/java/com/example/spring/controller/member/MemberController.java
+++ src/main/java/com/example/spring/controller/member/MemberController.java
@@ -8,29 +8,22 @@ import com.example.spring.service.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController{
-    @RequestMapping(value = "/member/input", method = RequestMethod.GET)
-    public void action(){
-    }
-
     @Autowired
     MemberService MemberService;
-    @RequestMapping(value = "/{action}", method = RequestMethod.GET)
-    public ModelAndView actionMethod(@RequestParam Map<String,Object> paramMap, ModelAndView ModelAndView){
-        String viewName = "/member/";
+
+    @RequestMapping(value = "/member/input")
+    public ModelAndView actionMethod(@RequestParam Map<String,Object> paramMap, ModelAndView modelAndView){
+        String viewName = "/member/input";
         Map<String,Object> resultMap = new HashMap<String,Object>();
 
-        if("read".equals(action)){
-            viewName = viewName + action;
-            resultMap = (Map<String,Object>) MemberService.getObject(paramMap);
-        }
-        ModelAndView.setViewName(viewName);
-        ModelAndView.addObject("resultMap", resultMap);
-        return ModelAndView;
+        resultMap = (Map<String,Object>)MemberService.getObject(paramMap);
+        modelAndView.setViewName(viewName);
+        modelAndView.addObject("resultMap", resultMap);
+        return modelAndView;
     }
 }
