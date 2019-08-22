f19431023dd99d1a405d517f4c3ce805df67af14
kibnm821 <kibnm821@naver.com>
2019-08-22 15:23:42 +0900

    v0.8.1

--- src/main/java/com/example/spring/controller/StudentController.java
+++ src/main/java/com/example/spring/controller/StudentController.java
@@ -2,23 +2,29 @@ package com.example.spring.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import com.example.spring.respository.StudentRepository;
+import com.example.spring.service.StudentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web .servlet.ModelAndView;
 
 @Controller
 public class StudentController{
     @Autowired
-    private StudentRepository repository;
-    @RequestMapping(value = "/student/lisk")
-    public ModelAndView actionMethod(ModelAndView modelAndView){
-        String viewName = "/student/lisk";
+    private StudentService service;
+    @RequestMapping(value = "/student/{action}")
+    public ModelAndView actionMethod(@RequestParam Map<String,Object> paramMap,@PathVariable String action,ModelAndView modelAndView){
+        String viewName = "/student/";
         List<Object> resultList = new ArrayList<Object>();
-        resultList = (List)repository.findAll();
+        if("lisk".equals(action)){
+            viewName = viewName + action;
+            resultList = (List<Object>)service.getObject(paramMap);
+        }
         modelAndView.setViewName(viewName);
         modelAndView.addObject("resultList", resultList);
         return modelAndView;
--- src/main/java/com/example/spring/service/StudentService.java
+++ src/main/java/com/example/spring/service/StudentService.java
@@ -1,4 +1,9 @@
-/*package com.example.spring.service;
+package com.example.spring.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.spring.respository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,8 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService{
     @Autowired
-
-    public Object getObject(Object dataMap){
-        Object resultoObject = 
+    private StudentRepository respository;
+    public Object getObject(Object paramMap){
+        Object resultObject = respository.findAll();
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        resultMap = (Map)respository.getOne((Long)paramMap.get("id"));
+        return resultObject;
     }
-}*/
+}
