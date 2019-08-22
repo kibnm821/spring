7da1c9121d47e5c1c994caea0812dbbe9ef47f5d
kibnm821 <kibnm821@naver.com>
2019-08-22 09:31:01 +0900

    Spring

--- src/main/java/com/example/spring/service/member/MemberService.java
+++ src/main/java/com/example/spring/service/member/MemberService.java
@@ -1,16 +1,16 @@
 package com.example.spring.service.member;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.example.spring.dao.ShareDao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService{
+    @Autowired
+    private ShareDao dao;
     public Object getObject(Object dataMap){
-        Map<String,Object> resultObject = new HashMap<String,Object>();
-        resultObject.put("MEMBER_ID", "2930293012012");
-        resultObject.put("NAME", "name 02");
+        Object resultObject = dao.getObject(dataMap);
         return resultObject;
     }
 }
